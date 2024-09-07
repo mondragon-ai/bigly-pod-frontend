4f8c59d4069878e22e088fef2cf38528cc35992b
mondragon-ai <angel.mondragon@yahoo.com>
2024-09-06 21:51:44 -0300

    🚀 patch & db push

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -17,7 +17,7 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 
   const billingCheck = await billing.require({
     plans: [USAGE_PLAN],
-    isTest: false,
+    isTest: true,
     onFailure: async () => billing.request({ plan: USAGE_PLAN }),
   });
 
